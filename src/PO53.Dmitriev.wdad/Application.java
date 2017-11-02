5106e16f2f984a3c96c1390c872c41696c2c19e9
DmitrievSemyon <bistrii.pylemetchik@gmail.com>
2017-11-02 08:43:04 +0400

    First lab. Final commit.

---
+++ src/PO53.Dmitriev.wdad/Application.java
@@ -0,0 +1,10 @@
+package PO53.Dmitriev.wdad;
+
+/**
+ * Created by semen on 28.09.2017.
+ */
+public class Application {
+    public static void main(String [] args){
+        System.out.println("I’m Semyon Dmitriev, and I’m not a monkey");
+    }
+}
